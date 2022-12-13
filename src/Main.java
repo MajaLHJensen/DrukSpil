9d50a612152d0b10cfb9ba59d038c02b37cbed81
MajaLHJensen <113036520+MajaLHJensen@users.noreply.github.com>
2022-12-13 11:01:06 +0100

    Fixed sql exception and made the questions

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
-import java.io.IOException;
-
+import java.sql.SQLException;
+import java.util.Scanner;
 public class Main {
+        static int count = 0;
 
-    public static void main(String[] args) throws IOException {
+        public Main()
+        {
+
+        }
+    public static void main(String[] args) throws SQLException
+    {
         // Here we call the method to welcome the player and log in or create an account.
-       TextUI textUI = new TextUI();
-        textUI.startGame();
-        textUI.gameSettup();
+       //TextUI textUI = new TextUI();
+        //textUI.startGame();
+        //textUI.gameSettup();
 
         //Here we call the method to start the game
         QuestionsDB questionsDB  = new QuestionsDB();
         questionsDB.getAllQuestions();
 
         // Here we call the method to save the players points and display them
-        Score score = new Score();
-        score.savePoint();
+       // Score score = new Score();
+        //score.savePoint();
     }
 }
--- src/QuestionsDB.java
+++ src/QuestionsDB.java
@@ -1,39 +1,33 @@
 import java.sql.*;
 import java.util.ArrayList;
+import java.sql.Connection;
 
 public class QuestionsDB {
     private Connection connection;
-    String url= "";
-    String username = "";
-    String password = "";
+    String url= "jdbc:mysql://127.0.0.1:3306/?user=root" + "autoReconnect=true&useSSL=false";
+    String username = "root";
+    String password = "1692";
     ArrayList<Questions>questionsData = new ArrayList<>();
 
-    public ArrayList<Questions> getAllQuestions() {
+    public ArrayList<Questions> getAllQuestions() throws SQLException
+    {
         establishConnection();
-        String query = "SELECT * from questions.mytable;";
-        try{
-            Statement statement = this.connection.createStatement();
-            statement.execute(query);
-            ResultSet resultSet = statement.getResultSet();
-            while (resultSet.next()){
-                String questionsID = resultSet.getString("questionID");
-                String question = resultSet.getString("question");
-                Questions questions = new Questions(questionsID, question);
-                questionsData.add(questions);
-                System.out.println(questions);
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-            System.out.println("This option does not exist. Please try again.");
+        String query = "SELECT * FROM question.mytable;";
+        Statement statement = this.connection.createStatement();
+        statement.execute(query);
+        ResultSet resultSet = statement.getResultSet();
+        while (resultSet.next()){
+            String questionsID = resultSet.getString("QuestionID");
+            String question = resultSet.getString("Questions");
+            Questions questions = new Questions(questionsID, question);
+            questionsData.add(questions);
+            System.out.println(questions);
         }
         return questionsData;
     }
 
-    private void establishConnection() {
-        try {
-            connection = DriverManager.getConnection(url, username, password);
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+    private void establishConnection() throws SQLException
+    {
+        connection = DriverManager.getConnection(url, username, password);
     }
 }
