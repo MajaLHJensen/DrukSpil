566897e970f75853a1bfa5af407f26856e87545c
MajaLHJensen <113036520+MajaLHJensen@users.noreply.github.com>
2022-12-13 13:00:34 +0100

    Fixed sql exception and made the questions

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException
+    {
         // Here we call the method to welcome the player and log in or create an account.
        TextUI textUI = new TextUI();
         textUI.startGame();
         textUI.gameSettup();
 
         //Here we call the method to start the game
-       // QuestionsDB questionsDB  = new QuestionsDB();
-        //questionsDB.getAllQuestions();
+       QuestionsDB questionsDB  = new QuestionsDB();
+       questionsDB.getAllQuestions();
 
         // Here we call the method to save the players points and display them
         //Score score = new Score();
